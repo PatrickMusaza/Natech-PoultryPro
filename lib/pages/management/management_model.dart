98e8159f3116a88dca95386dd01b5afe669a5290
Patrick-Poultry <poultryappacc@gmail.com>
2024-12-13 19:58:20 +0200

    initial

---
+++ lib/pages/management/management_model.dart
@@ -0,0 +1,32 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'management_widget.dart' show ManagementWidget;
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ManagementModel extends FlutterFlowModel<ManagementWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for Expandable widget.
+  late ExpandableController expandableExpandableController1;
+
+  // State field(s) for Expandable widget.
+  late ExpandableController expandableExpandableController2;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    expandableExpandableController1.dispose();
+    expandableExpandableController2.dispose();
+  }
+}
